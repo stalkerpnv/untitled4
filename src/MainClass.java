93f0703e3ce5ec785201df1a0cad9813b00a5e2f
stalker_pnv <stalker_pnv@mail.ru>
2022-02-28 17:18:20 +0900

    Change Codewe

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,7 @@
+import java.util.Random;
+
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Hi");
-        System.out.println("HI");
+        int []  arr = new int[5];
     }
 }
